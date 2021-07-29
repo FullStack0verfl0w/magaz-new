0b4919716f84571dbf4737f7b3624edb4bef0aa7
pBinarySoul <pBinarySoul@gmail.com>
2021-07-29 16:00:50 +0300

    Changed header's height

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,10 @@
+import Constants from "expo-constants";
+
 export const STORE_ADDRESS = "http://edoprovod.ru/";
 export const DATABASE_NAME = "magaz";
 export const DATABASE_VERSION = 2;
 export const AUTH_TOKEN_EXPIRE_TIME = 300000; // in ms
-export const HEADER_HEIGHT = 96;
+export const HEADER_HEIGHT = 76 + Constants.statusBarHeight;
 
 export const HeaderStyle = (backgroundColor = "#fff", style = {}) => {
 	return {
